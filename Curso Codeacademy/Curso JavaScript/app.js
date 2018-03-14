895bfac39b1d585a3b7adbd6396a86dc04061bee
Claudio Moreira <claudiomoreira22@gmail.com>
2018-03-14 14:53:21 +0000

    t

---
+++ Curso Codeacademy/Curso JavaScript/app.js
@@ -0,0 +1,29 @@
+//Imprimir strings, valores e booleans
+console.log('Culaudio');
+console.log(13.4);
+console.log(false);
+
+//Operações matematicas
+console.log(19 + 3.5);
+console.log(2018 - 1968);
+console.log(69 / 216);
+console.log(12 * 11);
+
+//Comprimento da string
+console.log('Se amanha nao chover vai estar bom tempo'.length());
+
+// Log Codecademy in all uppercase letters
+console.log('Codecademy'.toUpperCase()); 
+
+// Use a string method to log the following statment without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+
+//Math.random generates a random number between 0 and 1. 
+//Then, Math.floor rounds the number down to the nearest whole number.
+console.log(Math.floor(Math.random() * 100));
+
+//Math.ceil returns the smallest integer greater than or equal to a decimal number.
+console.log(Math.ceil(43.8));
+
+//Number.isInteger checks if a number is an integer.
+console.log(Number.isInteger(2017));
